17d035cacc0b10dbd4bc469cf4f26191f9ecaa3d
timonson <timo.rossa@tutanota.com>
2019-10-12 17:41:13 +0200

    made code more readable

--- validate.ts
+++ validate.ts
@@ -2,17 +2,18 @@ import { toUint8Array as convertBase64ToUint8Array } from "https://denopkg.com/c
 import { addPaddingToBase64url } from "https://denopkg.com/timonson/base64url/base64url.ts"
 import makeJwt, { Claims, Jose } from "./create.ts"
 
-interface CritHandlers {
+interface Handlers {
   [key: string]: (header?: any) => any
 }
 
 /*
- * The "alg" (algorithm) Header Parameter identifies the cryptographic algorithm
- * The 'alg' header MUST be present (JWS §4.1.1)
+ * The "alg" header parameter identifies the cryptographic algorithm and MUST
+ * be present (JWS §4.1.1)
  */
-function checkAlgHeaderParameter(header: Jose, algorithms: string[]): string {
+function checkHeaderAlg(header: Jose, algorithms: string[]): string {
+  if (!header.alg) throw ReferenceError("header parameter 'alg' is empty")
   const algorithm = algorithms.find(el => el === header.alg)
-  if (!algorithm) throw RangeError("no or no matching algorithm in the header")
+  if (!algorithm) throw RangeError("no matching crypto algorithm in the header")
   return algorithm
 }
 
@@ -20,44 +21,33 @@ function checkAlgHeaderParameter(header: Jose, algorithms: string[]): string {
  * A present 'crit' header parameter indicates that the JWS signature validator
  * must understand and process additional claims (JWS §4.1.11)
  */
-function checkCritHeaderParameter(
-  header: Jose,
-  critHandlers: CritHandlers
-): Promise<any[]> {
+function checkHeaderCrit(header: Jose, critHandlers: Handlers): Promise<any[]> {
   // prettier-ignore
   const reservedNames = new Set([ 
     "alg", "jku", "jwk", "kid", "x5u", "x5c", "x5t", "x5t#S256", "typ", "cty",
     "crit", "enc", "zip", "epk", "apu", "apv", "iv", "tag", "p2s", "p2c",
   ])
-
   if (
     !Array.isArray(header.crit) ||
     header.crit.some(str => typeof str !== "string" || !str)
   )
-    throw TypeError(
-      '"crit" header parameter must be an array of non-empty strings'
-    )
+    throw Error("header parameter 'crit' must be an array of non-empty strings")
   if (header.crit.some(str => reservedNames.has(str)))
-    throw Error(`the 'crit' list contains a non-extension header parameter`)
-  const activatedHandlers = header.crit
-    .filter(str => typeof critHandlers[str] === "function")
-    .map(str => header => critHandlers[str](header[str]))
-  if (activatedHandlers.length !== header.crit.length)
+    throw Error("the 'crit' list contains a non-extension header parameter")
+  if (header.crit.some(str => typeof critHandlers[str] !== "function"))
     throw Error("critical extension header parameters are not understood")
-  return Promise.all(activatedHandlers.map(handler => handler(header)))
+  return Promise.all(header.crit.map(str => critHandlers[str](header[str])))
 }
 
 function handleHeader(
   header: Jose,
   algorithms: string[],
-  critHandlers: CritHandlers
+  critHandlers: Handlers
 ): [string, Promise<any[]>] {
-  if (typeof header !== "object")
-    throw TypeError("the json header is no object")
-  const algorithm: string = checkAlgHeaderParameter(header, algorithms)
+  const algorithm: string = checkHeaderAlg(header, algorithms)
   const critResults: Promise<any[]> =
     "crit" in header
-      ? checkCritHeaderParameter(header, critHandlers)
+      ? checkHeaderCrit(header, critHandlers)
       : Promise.resolve([])
   return [algorithm, critResults]
 }
@@ -91,15 +81,15 @@ function validateJwt(
   jwt: string,
   key: string,
   throwErrors: boolean = true,
-  critHandlers: CritHandlers = {}
+  critHandlers: Handlers = {}
 ): Promise<any[]> | void {
   const algorithms: string[] = ["HS256", "HS512", "none"]
   try {
     if (!/^[A-Za-z0-9_-]+\.[A-Za-z0-9_-]*\.[A-Za-z0-9_-]*$/.test(jwt))
       throw Error("wrong type or format")
     const [header, payload, oldSig] = parseDecode(jwt)
-    const [alg, critResults] = handleHeader(header, algorithms, critHandlers)
     if (payload && payload.exp) checkIfExpired(payload.exp)
+    const [alg, critResults] = handleHeader(header, algorithms, critHandlers)
     const newSig = parseDecode(makeJwt(header, payload, key))[2]
     if (oldSig === newSig) return critResults
     else throw Error("signatures don't match")
