3725a8e6c398081ce1962d94bf6a8ca2d7c452de
alexander <alexander.galushka@verizonmedia.com>
2020-07-13 10:04:17 -0700

    fix: use deno std lib functions

--- base64/README.md
+++ base64/README.md
@@ -26,29 +26,3 @@ const uint8Array = new TextEncoder().encode(str)
 uint8Array === convertBase64ToUint8Array(convertUint8ArrayToBase64(uint8Array))
 str === convertBase64ToString(convertStringToBase64(str))
 ```
-
-## base64url
-
-#### convertBase64ToBase64url(base64: string): string
-
-The `+` and `/` characters of standard **base64** are respectively replaced by
-`-` and `_` and the padding `=` characters are removed.
-
-#### convertBase64urlToBase64(base64url: string): string
-
-Converts a **base64url** string to standard **base64**.
-
-#### addPaddingToBase64url(base64url: string): string
-
-The `addPaddingToBase64url` function makes the string length a _multiple of 4_
-by adding the padding character (`=`).  
-More about this [here](https://en.wikipedia.org/wiki/Base64#URL_applications).
-
-### Example
-
-```typescript
-const base64 = "c3ViamVjdHM/X2Q9MQ=="
-const base64url = convertBase64ToBase64url(base64)
-
-console.log(base64url) // c3ViamVjdHM_X2Q9MQ
-```
--- base64/base64url.ts
+++ base64/base64url.ts
@@ -1,36 +1,21 @@
 import {
   convertBase64ToUint8Array,
   convertUint8ArrayToBase64,
-} from "./base64.ts"
-
-function addPaddingToBase64url(base64url: string): string {
-  if (base64url.length % 4 === 2) return base64url + "=="
-  if (base64url.length % 4 === 3) return base64url + "="
-  if (base64url.length % 4 === 1)
-    throw new TypeError("Illegal base64url string!")
-  return base64url
-}
-
-function convertBase64urlToBase64(base64url: string): string {
-  return addPaddingToBase64url(base64url).replace(/\-/g, "+").replace(/_/g, "/")
-}
-
-function convertBase64ToBase64url(base64: string): string {
-  return base64.replace(/=/g, "").replace(/\+/g, "-").replace(/\//g, "_")
-}
+} from "./base64.ts";
+import {
+  convertBase64urlToBase64,
+  convertBase64ToBase64url,
+} from "https://deno.land/std@v0.59.0/encoding/base64url.ts";
 
 function convertBase64urlToUint8Array(base64url: string): Uint8Array {
-  return convertBase64ToUint8Array(convertBase64urlToBase64(base64url))
+  return convertBase64ToUint8Array(convertBase64urlToBase64(base64url));
 }
 
 function convertUint8ArrayToBase64url(uint8Array: Uint8Array): string {
-  return convertBase64ToBase64url(convertUint8ArrayToBase64(uint8Array))
+  return convertBase64ToBase64url(convertUint8ArrayToBase64(uint8Array));
 }
 
 export {
-  convertBase64ToBase64url,
-  convertBase64urlToBase64,
-  addPaddingToBase64url,
   convertBase64urlToUint8Array,
   convertUint8ArrayToBase64url,
-}
+};
