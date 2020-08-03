d07ff5b6a7e3f8d8b3ed0dab180eb84d26c8bc0d
timonson <54777088+timonson@users.noreply.github.com>
2020-08-04 00:04:36 +0200

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export { decodeString as convertHexToUint8Array } from "https://deno.land/std@v0.63.0/encoding/hex.ts";
-export { HmacSha256 } from "https://deno.land/std@v0.63.0/hash/sha256.ts";
-export { HmacSha512 } from "https://deno.land/std@v0.63.0/hash/sha512.ts";
-export { addPaddingToBase64url } from "https://deno.land/std@v0.63.0/encoding/base64url.ts";
+export { decodeString as convertHexToUint8Array } from "https://deno.land/std@0.63.0/encoding/hex.ts";
+export { HmacSha256 } from "https://deno.land/std@0.63.0/hash/sha256.ts";
+export { HmacSha512 } from "https://deno.land/std@0.63.0/hash/sha512.ts";
+export { addPaddingToBase64url } from "https://deno.land/std@0.63.0/encoding/base64url.ts";
