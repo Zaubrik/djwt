8bd13785a3bfdc44480816068ebc231d9b9f8165
Marius Vatasoiu <marius.vatasoiu@outlook.com>
2020-09-16 08:31:00 +0300

    style: run fmt

--- deps.ts
+++ deps.ts
@@ -1,4 +1,7 @@
-export { decodeString as convertHexToUint8Array, encodeToString as convertUint8ArrayToHex } from "https://deno.land/std@0.69.0/encoding/hex.ts";
+export {
+  decodeString as convertHexToUint8Array,
+  encodeToString as convertUint8ArrayToHex,
+} from "https://deno.land/std@0.69.0/encoding/hex.ts";
 export { HmacSha256 } from "https://deno.land/std@0.69.0/hash/sha256.ts";
 export { HmacSha512 } from "https://deno.land/std@0.69.0/hash/sha512.ts";
 export { addPaddingToBase64url } from "https://deno.land/std@0.69.0/encoding/base64url.ts";
