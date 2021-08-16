417f4fe746a3a253465453464730269bcb0c285d
timonson <timo.rossa@tutanota.com>
2021-08-16 19:21:02 +0200

    Implement new Web Crypto API

--- examples/example_deps.ts
+++ examples/example_deps.ts
@@ -1,3 +1,1 @@
-export { serve } from "https://deno.land/std@0.85.0/http/server.ts";
-export { decode, encode } from "https://deno.land/std@0.85.0/encoding/utf8.ts";
-export { dirname, fromFileUrl } from "https://deno.land/std@0.85.0/path/mod.ts";
+export { serve } from "https://deno.land/std@0.104.0/http/server.ts";
