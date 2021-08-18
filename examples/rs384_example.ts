9a3f3adee61543259a1cad7ca8f7034456597fb1
timonson <timo.rossa@tutanota.com>
2021-08-18 09:46:53 +0200

    Improve examples

--- examples/rs384_example.ts
+++ examples/rs384_example.ts
@@ -3,7 +3,7 @@ import { serve } from "./example_deps.ts";
 
 import type { Header, Payload } from "../mod.ts";
 
-const key = await window.crypto.subtle.generateKey(
+const { privateKey, publicKey } = await window.crypto.subtle.generateKey(
   {
     name: "RSASSA-PKCS1-v1_5",
     modulusLength: 4096,
@@ -29,11 +29,11 @@ console.log("server is listening at 0.0.0.0:8000");
 for await (const req of serve("0.0.0.0:8000")) {
   if (req.method === "GET") {
     req.respond({
-      body: (await create(header, payload, key.privateKey)) + "\n",
+      body: (await create(header, payload, privateKey)) + "\n",
     });
   } else {
     const jwt = new TextDecoder().decode(await Deno.readAll(req.body));
-    await verify(jwt, key.publicKey).then(() =>
+    await verify(jwt, publicKey).then(() =>
       req.respond({ body: "Valid JWT\n" })
     )
       .catch(() => req.respond({ body: "Invalid JWT\n", status: 401 }));
